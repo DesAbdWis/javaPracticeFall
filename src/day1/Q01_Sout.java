c67f74bb27a0821081efedac84cc19d6ec60310e
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-06-20 15:09:47 +0300

    Initial commit

---
+++ src/day1/Q01_Sout.java
@@ -0,0 +1,26 @@
+package day1;
+
+public class Q01_Sout {
+     /*
+  Konsolda asagidaki ciktiyi yazdiriniz
+
+        T
+        E
+        C
+        H
+        P
+        R
+        O
+
+        E
+        D
+        U
+        C
+        A
+        T
+        I
+        O
+        N
+         */
+
+}
