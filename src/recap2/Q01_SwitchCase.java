3b01f0e73effb4516df652e68f628af78798a47a
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-06-28 23:19:56 +0300

    28.06.22

--- src/recap2/Q01_SwitchCase.java
+++ src/recap2/Q01_SwitchCase.java
@@ -15,4 +15,27 @@ public class Q01_SwitchCase {
     aksi halde: izin gunu
     (if deyimini KULLANMAYIN)
 */
+    public static void main(String[] args) {
+        String gun = "Sali";
+
+        switch (gun) {
+            case "Pazartesi":
+            case "Sali":
+                System.out.println("java dersi");
+                break;
+            case "Carsamba":
+            case "Cumartesi":
+                System.out.println("Sql dersi");
+                break;
+            case "Persembe":
+            case "Cuma":
+                System.out.println("Selenyum dersi");
+                break;
+
+            default:
+                System.out.println("izin gunu");
+        }
+    }
+
+
 }
