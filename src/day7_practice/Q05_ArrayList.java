b253459728b73bdb87199e984aab8ad2477f3972
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-07-19 18:24:36 +0300

    19.07.22

--- src/day7_practice/Q05_ArrayList.java
+++ src/day7_practice/Q05_ArrayList.java
@@ -1,5 +1,10 @@
 package day7_practice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Q05_ArrayList {
 
     /*  Rastgele kullanici adi olusturan JAVA kodu yaziniz.
@@ -10,4 +15,35 @@ public class Q05_ArrayList {
      *  5. Eger bu kullanici adi zaten varsa, sonuna rastgele sayi olusturup ekleyelim, ve gosterelim.
 
      */
+    public static void main(String[] args) {
+        List<String> databaseIsimler = new ArrayList<>();
+        databaseIsimler.add("Enes");
+        databaseIsimler.add("Ismail");
+        databaseIsimler.add("AbdulBaki");
+        databaseIsimler.add("Emre");
+        databaseIsimler.add("Melisa");
+        databaseIsimler.add("Oguz");
+        System.out.println("databaseIsimler = " + databaseIsimler);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("kullanmak istediginiz username i giriniz :");
+        String username = scan.nextLine().trim();
+
+        boolean usernameVarmi = databaseIsimler.contains(username);
+
+        if(usernameVarmi){
+            System.out.println("bu kullanici adi zaten alinmis");
+        }else System.out.println("bu kullanici adini kullanabilirsiniz");
+
+        if(usernameVarmi){
+            int randomSayi = new Random().nextInt(100);
+            username = username + "" + randomSayi;
+            System.out.println("yeni kullanici adiniz :" + username);
+            databaseIsimler.add(username);
+            System.out.println("databaseIsimler = " + databaseIsimler);
+        }else{
+            System.out.println("yeni kullanici adiniz :" + username);
+        }
+    }
+
 }
