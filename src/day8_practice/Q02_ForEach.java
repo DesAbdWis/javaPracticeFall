3db17cb78dc38df6e645801d22191e2c3f9e5079
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-07-22 20:31:59 +0300

    22.07.22

---
+++ src/day8_practice/Q02_ForEach.java
@@ -0,0 +1,12 @@
+package day8_practice;
+
+public class Q02_ForEach {
+
+    // String[] list = {"fruits", "vegetables", "meat", "milk"};
+    // print array with for each loop
+    // print length of each element
+
+    // Part 2:
+    // Print all the elements from array using for eachLoop
+    // If an element starts with 'v' then quit the loop
+}
